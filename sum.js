6ea62e107e49ae6551789dcfc45c5f200ee44226
helal9380 <asadurrahman9380@gmail.com>
2024-02-02 09:49:13 +0600

    added the sum of array in js

---
+++ sum.js
@@ -0,0 +1,12 @@
+
+// Sum of Array Elements:
+// Calculate the sum of all elements in the array arr.
+
+let numbers = [10, 50, 25, 36, 24];
+let sum = 0;
+
+for(const number of numbers) {
+    sum = sum += number;
+}
+console.log(sum)
+// output : 145
